70e546a55d12b26c7bac06a71232e37eee34e562
Aman Srivastava <59829331+amans118@users.noreply.github.com>
2020-05-13 05:07:58 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+docker \
+  run \
+  --detach \
+  --env MYSQL_ROOT_PASSWORD=wordpress \
+  --env MYSQL_USER=wordpress \
+  --env MYSQL_PASSWORD=wordpress \
+  --env MYSQL_DATABASE=wordpress \
+  --name mysql \
+  --publish 3306:3306 \
+  --network wordpressonly \
+  mysql:5.7
+
+# downloading Dockerfile from github
+wget https://raw.githubusercontent.com/amans118/docker-only-wordpress/master/Dockerfile
+
+# building image
+docker build -t dockeronlywp:v1 --no-cache .
+
+# running container
+docker \
+  run \
+  --detach \
+  --name dockeronlywp \
+  --publish 9001:80 \
+  --network wordpressonly \
+  dockeronlywp:v1
